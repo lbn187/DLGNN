4db35f11a98dc53ea79ff549d1fe966ceb6ac610
Boring Lee <36894888+lbn187@users.noreply.github.com>
2020-11-17 16:23:41 +0800

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -3,6 +3,7 @@ import os
 from torch_geometric.utils import negative_sampling
 from ogb.linkproppred import PygLinkPropPredDataset
 import argparse
+import random
 import torch_geometric.transforms as T
 def gpu_setup(use_gpu, gpu_id):
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
@@ -16,8 +17,9 @@ def gpu_setup(use_gpu, gpu_id):
     return device
 def main():
     parser = argparse.ArgumentParser(description='Process-Data')
-    parser.add_argument('--name', type=str, default='ogbl-ddi')
-    parser.add_argument('--dir', type=str, default='all.txt')
+    parser.add_argument('--name', type=str, default='ogbl-ppa')
+    parser.add_argument('--dir', type=str, default='/blob2/v-bonli/data/ppa_edges.txt')
+    parser.add_argument('--use_valedges_as_input', type=bool, default=False)
     parser.add_argument('--gpu', type=int, default=0)
     args = parser.parse_args()
     if args.name.startswith('ogbl'):
@@ -74,6 +76,17 @@ def main():
                     f.write(str(x.item())+"\n")
                 f.close()
             return
+        edge_index = data.edge_index
+        valid_pos_edge = split_edge['valid']['edge']
+        valid_neg_edge = split_edge['valid']['edge_neg']
+        test_pos_edge = split_edge['test']['edge']
+        test_neg_edge = split_edge['test']['edge_neg']
+        if args.use_valedges_as_input:
+            val_edge_index = valid_pos_edge.t()
+            full_edge_index = torch.cat([edge_index, val_edge_index], dim=-1)
+            data.adj_t = SparseTensor.from_edge_index(full_edge_index).t()
+            data.adj_t = data.adj_t.to_symmetric()
+            train_pos_edge = torch.cat([train_pos_edge, valid_pos_edge], dim = 1)
         if args.name == 'ogbl-ddi':
             adj_t = data.adj_t.to(device)
             row, col, _ = adj_t.coo()
@@ -81,10 +94,6 @@ def main():
             train_neg_edge = negative_sampling(edge_index, num_nodes = data.num_nodes, num_neg_samples = train_pos_edge.size(0), method = 'dense').t()
         else:
             train_neg_edge = torch.randint(0, data.num_nodes, train_pos_edge.size(), dtype=torch.long)
-        valid_pos_edge = split_edge['valid']['edge']
-        valid_neg_edge = split_edge['valid']['edge_neg']
-        test_pos_edge = split_edge['test']['edge']
-        test_neg_edge = split_edge['test']['edge_neg']
         f = open(args.dir, "w")
         f.write(str(data.num_nodes)+"\n")
         f.write(str(train_pos_edge.size(0))+"\n")
@@ -92,9 +101,22 @@ def main():
         for x in ret:
             f.write(str(x.item())+"\n")
         f.write(str(train_neg_edge.size(0))+"\n")
-        ret = train_neg_edge.reshape(-1)
-        for x in ret:
-            f.write(str(x.item())+"\n")
+        ww1 = 0
+        ww2 = 1
+        for x in train_neg_edge:
+            v1, v2 = x
+            w1 = v1.item()
+            w2 = v2.item()
+            while w1 == w2:
+                if args.name == 'ogbl-ddi':
+                    w1, w2 = ww1, ww2
+                else:
+                    w1 = random.randint(0, data.num_nodes - 1)
+                    w2 = random.randint(0, data.num_nodes - 1)
+            f.write(str(w1)+"\n")
+            f.write(str(w2)+"\n")
+            ww1 = w1
+            ww2 = w2
         f.write(str(valid_pos_edge.size(0))+"\n")
         ret = valid_pos_edge.reshape(-1)
         for x in ret:
