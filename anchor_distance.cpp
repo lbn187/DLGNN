9bfe528e657d760960164edfb7faf3dafb54597c
lbn187 <lbn187@github.com>
2020-12-11 08:20:59 +0000

    new_anchor

--- anchor_distance.cpp
+++ anchor_distance.cpp
@@ -8,7 +8,7 @@ int R(int maxv){
 	int y = rand() % 32768;
 	return (x * 32768 + y) % maxv;
 }
-void anchor_distance(char *in_dir, char *out_dir, int num, int maxv, int extra_random_edges, bool use_val){
+void anchor_distance(char *in_dir, char *out_dir, int num, int maxv, bool use_min, int extra_random_edges, bool use_val){
 	srand(time(0));
 	int num_nodes, train_pos_m, train_neg_m, valid_pos_m, valid_neg_m, test_pos_m, test_neg_m;
 	vector<P> train_pos, train_pos2, train_neg, valid_pos, valid_neg, test_pos, test_neg;
@@ -26,6 +26,7 @@ void anchor_distance(char *in_dir, char *out_dir, int num, int maxv, int extra_r
 		scanf("%d%d", &x, &y);
 		train_pos[i].x = x;
 		train_pos[i].y = y;
+		if(use_min)train_neg[i].z = maxv * 2;
 		if(extra_random_edges == 0){
 			edge[x].push_back(y);
 			edge[y].push_back(x);
@@ -33,24 +34,34 @@ void anchor_distance(char *in_dir, char *out_dir, int num, int maxv, int extra_r
 	}
 	scanf("%d", &train_neg_m);
 	train_neg.resize(train_neg_m);
-	for(int i = 0; i < train_neg_m; i++)
+	for(int i = 0; i < train_neg_m; i++){
 		scanf("%d%d", &train_neg[i].x, &train_neg[i].y);
+		if(use_min)train_neg[i].z = maxv * 2;
+	}
 	scanf("%d", &valid_pos_m);
 	valid_pos.resize(valid_pos_m);
-	for(int i = 0; i < valid_pos_m; i++)
+	for(int i = 0; i < valid_pos_m; i++){
 		scanf("%d%d", &valid_pos[i].x, &valid_pos[i].y);
+		if(use_min)valid_pos[i].z = maxv * 2;
+	}
 	scanf("%d", &valid_neg_m);
 	valid_neg.resize(valid_neg_m);
-	for(int i = 0; i < valid_neg_m; i++)
+	for(int i = 0; i < valid_neg_m; i++){
 		scanf("%d%d", &valid_neg[i].x, &valid_neg[i].y);
+		if(use_min)valid_neg[i].z = maxv * 2;
+	}
 	scanf("%d",&test_pos_m);
 	test_pos.resize(test_pos_m);
-	for(int i = 0; i < test_pos_m; i++)
+	for(int i = 0; i < test_pos_m; i++){
 		scanf("%d%d", &test_pos[i].x, &test_pos[i].y);
+		if(use_min)test_pos[i].z = maxv * 2;
+	}
 	scanf("%d",&test_neg_m);
 	test_neg.resize(test_neg_m);
-	for(int i = 0; i < test_neg_m; i++)
+	for(int i = 0; i < test_neg_m; i++){
 		scanf("%d%d", &test_neg[i].x, &test_neg[i].y);
+		if(use_min)test_neg[i].z = maxv * 2;
+	}
 	fclose(stdin);
 	for(int i = 0; i < num_nodes; i++)
 		vis[i] = -1;
@@ -85,13 +96,17 @@ void anchor_distance(char *in_dir, char *out_dir, int num, int maxv, int extra_r
 				}
 		}
 		for(int j = 0; j < train_pos_m; j++)
-			train_pos[j].z += d[train_pos[j].x] + d[train_pos[j].y];
+			if(use_min)train_pos[j].z = min(train_pos[j].z, d[train_pos[j].x] + d[train_pos[j].y]);
+			else train_pos[j].z += d[train_pos[j].x] + d[train_pos[j].y];
 		for(int j = 0; j < train_neg_m; j++)
-			train_neg[j].z += d[train_neg[j].x] + d[train_neg[j].y];
+			if(use_min)train_neg[j].z = min(train_neg[j].z, d[train_neg[j].x] + d[train_neg[j].y]);
+			else train_neg[j].z += d[train_neg[j].x] + d[train_neg[j].y];
 		for(int j = 0; j < valid_pos_m; j++)
-			valid_pos[j].z += d[valid_pos[j].x] + d[valid_pos[j].y];
+			if(use_min)valid_pos[j].z = min(valid_pos[j].z, d[valid_pos[j].x] + d[valid_pos[j].y]);
+			else valid_pos[j].z += d[valid_pos[j].x] + d[valid_pos[j].y];
 		for(int j = 0; j < valid_neg_m; j++)
-			valid_neg[j].z += d[valid_neg[j].x] + d[valid_neg[j].y];
+			if(use_min)valid_neg[j].z = min(valid_neg[j].z, d[valid_neg[j].x] + d[valid_neg[j].y]);
+			else valid_neg[j].z += d[valid_neg[j].x] + d[valid_neg[j].y];
 		if(use_val){
 			for(int j = 0; j < valid_pos_m; j++){
 				edge[valid_pos[j].x].push_back(valid_pos[j].y);
@@ -116,9 +131,11 @@ void anchor_distance(char *in_dir, char *out_dir, int num, int maxv, int extra_r
 			}
 		}
 		for(int j = 0; j < test_pos_m; j++)
-			test_pos[j].z += d[test_pos[j].x] + d[test_pos[j].y];
+			if(use_min)test_pos[j].z = min(test_pos[j].z, d[test_pos[j].x] + d[test_pos[j].y]);
+			else test_pos[j].z += d[test_pos[j].x] + d[test_pos[j].y];
 		for(int j = 0; j < test_neg_m; j++)
-			test_neg[j].z += d[test_neg[j].x] + d[test_neg[j].y];
+			if(use_min)test_neg[j].z = min(test_neg[j].z, d[test_neg[j].x] + d[test_neg[j].y]);
+			else test_neg[j].z += d[test_neg[j].x] + d[test_neg[j].y];
 		if(extra_random_edges > 0 || use_val){
 			for(vector<int> x : edge){
 				x.clear();
--- anchor_distance.hpp
+++ anchor_distance.hpp
@@ -1,3 +1,3 @@
 #include<bits/stdc++.h>
 using namespace std;
-void anchor_distance(char *in_dir, char *out_dir, int num, int maxv, int extra_random_edges, bool use_val);
+void anchor_distance(char *in_dir, char *out_dir, int num, int maxv, bool use_min, int extra_random_edges, bool use_val);
